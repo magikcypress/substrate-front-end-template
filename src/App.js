d6fe2c10e5ff8a12634f3d55971ce3ab470b5415
magikcypress <cyp@rouquin.me>
2022-06-05 20:03:07 +0200

    Add avatar img

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import NodeInfo from './NodeInfo'
 import TemplateModule from './TemplateModule'
 import Transfer from './Transfer'
 import Upgrade from './Upgrade'
+import Kitties from './Kitties'
 
 function Main() {
   const { apiState, apiError, keyringState } = useSubstrateState()
@@ -83,6 +84,9 @@ function Main() {
           </Grid.Row>
           <Grid.Row>
             <TemplateModule />
+          </Grid.Row>
+          <Grid.Row>
+            <Kitties />
           </Grid.Row>
         </Grid>
       </Container>
